1743d7e358fbca7dd4331129c077fe13e492a5f2
wishcraft <wishcraft@c17157d5-08f2-41e7-a131-df055f6a0da3>
2011-07-27 22:53:26 +0000

    PHPDoc 1.01 - PHP Documentor like php.net for online code discussions

--- releases/1.01 - prealpha/htdocs/modules/phpdoc/class/path.php
+++ releases/1.01 - prealpha/htdocs/modules/phpdoc/class/path.php
@@ -10,14 +10,72 @@ class PhpdocPath extends XoopsObject
     function PhpdocPath($fid = null)
     {
         $this->initVar('pathid', XOBJ_DTYPE_INT, 0, false);
+        $this->initVar('projectid', XOBJ_DTYPE_INT, 0, false);
+        $this->initVar('fileid', XOBJ_DTYPE_INT, 0, false);
         $this->initVar('folder', XOBJ_DTYPE_TXTBOX, '', false, 128);
         $this->initVar('path', XOBJ_DTYPE_TXTBOX, '', false, 500);
+        $this->initVar('relative', XOBJ_DTYPE_ENUM, '_MI_PHPDOC_PROJECTS_PATH', false, false, false, array('_MI_PHPDOC_XOOPS_ROOT_PATH','_MI_PHPDOC_XOOPS_UPLOAD_PATH','_MI_PHPDOC_XOOPS_VAR_PATH','_MI_PHPDOC_XOOPS_TRUST_PATH','_MI_PHPDOC_XOOPS_MODULE_PATH','_MI_PHPDOC_PROJECTS_PATH','_MI_PHPDOC_OPEN_PATH'));
         $this->initVar('itemid', XOBJ_DTYPE_INT, 0, false);
         $this->initVar('md5', XOBJ_DTYPE_TXTBOX, 0, false, 32);
 		$this->initVar('created', XOBJ_DTYPE_INT, 0, false);
 		$this->initVar('updated', XOBJ_DTYPE_INT, 0, false);
+		$this->initVar('actioned', XOBJ_DTYPE_INT, 0, false);
 	}
 	
+	function getPluginName() {
+		$ret = '';
+		if (defined($this->getVar('relative').'_PLUGIN'))
+			$ret .= constant($this->getVar('relative').'_PLUGIN');
+		return $ret;				
+	}
+	
+	function runPreInsertPlugin($handler) {
+		
+		include_once($GLOBALS['xoops']->path('/modules/phpdoc/plugins/'.strtolower($this->getPluginName()).'.php'));
+		
+		switch ($this->getVar('relative')) {
+			default:
+				$func = $this->getPluginName().'PreInsertPath';
+				break;
+		}
+		
+		if (function_exists($func))  {
+			return @$func($this, $handler);
+		}
+		return $this;
+	}
+
+	function runPostInsertPlugin($handler) {
+		
+		include_once($GLOBALS['xoops']->path('/modules/phpdoc/plugins/'.strtolower($this->getPluginName()).'.php'));
+		
+		switch ($this->getVar('relative')) {
+			default:
+				$func = $this->getPluginName().'PostInsertPath';
+				break;
+		}
+				
+		if (function_exists($func))  {
+			return @$func($this, $handler);
+		}
+		return $this;
+	}
+	
+	function runPostGetPlugin($handler) {
+		
+		include_once($GLOBALS['xoops']->path('/modules/phpdoc/plugins/'.strtolower($this->getPluginName()).'.php'));
+		
+		switch ($this->getVar('relative')) {
+			default:
+				$func = $this->getPluginName().'PostGetPath';
+				break;
+		}
+				
+		if (function_exists($func))  {
+			return @$func($this, $handler);
+		}
+		return $this;
+	}
 }
 
 
@@ -51,8 +109,39 @@ class PhpdocPathHandler extends XoopsPersistableObjectHandler
     		$obj->setVar('md5', $md5);
     	}
     	
-    	return parent::insert($obj, $force);
+    	$run_plugin = false;
+    	if ($obj->vars['relative']['changed']==true) {	
+			$obj->setVar('actioned', time());
+			$run_plugin = true;
+		}
+    	if ($run_plugin==true) {
+    		@$obj->runPreInsertPlugin($this);
+    	}
+		$id = parent::insert($obj, $force);
+    	if ($run_plugin==true) {
+    		$obj = parent::get($id);
+    		@$obj->runPostInsertPlugin($this);
+    	}
+    	return $id;
     }
+     
+    function get($id) 
+    {
+    	$obj = parent::get($id);
+    	if (is_object($obj)) {
+    		return @$obj->runPostGetPlugin($this);
+    	}
+    }
+    
+	function getObjects($criteria, $id_as_key=false, $as_object=true) 
+    {
+    	$objs = parent::getObjects($criteria, $id_as_key, $as_object);
+    	if ($as_object==true) {
+    		foreach($objs as $key => $obj)
+    			$objs[$key] = @$obj->runPostGetPlugin($this);
+    	}
+    	return $objs;
+    } 
      
 }
 ?>
---
+++ releases/1.01 - prealpha/htdocs/modules/phpdoc/language/english/admin.php
@@ -0,0 +1,54 @@
+<?php
+
+	// Table Header Items
+	define('_AM_PHPDOC_TH_ACTIONS', 'Actions');
+	define('_AM_PHPDOC_TH_CID', 'Category');
+	define('_AM_PHPDOC_TH_PARENTID', 'Parent');
+	define('_AM_PHPDOC_TH_WEIGHT', 'Weight');
+	define('_AM_PHPDOC_TH_ITEMID', 'Item ID');
+	define('_AM_PHPDOC_TH_MD5', 'Record Fingeprint');
+	define('_AM_PHPDOC_TH_CREATED', 'Created');
+	define('_AM_PHPDOC_TH_UPDATED', 'Updated');
+	define('_AM_PHPDOC_TH_ACTIONED', 'Actioned');
+	define('_AM_PHPDOC_TH_PROJECTID', 'Project ID');
+	define('_AM_PHPDOC_TH_STATUS', 'Status');
+	define('_AM_PHPDOC_TH_URL', 'URL');
+	define('_AM_PHPDOC_TH_FILEID', 'File ID');
+	define('_AM_PHPDOC_TH_VERSIONID', 'Version ID');
+	define('_AM_PHPDOC_TH_BASEPATHID', 'Base tier in path');
+	define('_AM_PHPDOC_TH_SECONDPATHID', '2nd tier in path');
+	define('_AM_PHPDOC_TH_THIRDPATHID', '3rd tier in path');
+	define('_AM_PHPDOC_TH_FORTHPATHID', '4th tier in path');
+	define('_AM_PHPDOC_TH_PATH', 'Path');
+	define('_AM_PHPDOC_TH_FILENAME', 'Filename');
+	define('_AM_PHPDOC_TH_CLASSES', 'Classes');
+	define('_AM_PHPDOC_TH_FUNCTIONS', 'Functions');
+	define('_AM_PHPDOC_TH_FILEMD5', 'Fingerprint');
+	define('_AM_PHPDOC_TH_CLASSID', 'Class ID');
+	define('_AM_PHPDOC_TH_CIDS', 'Categories');
+	define('_AM_PHPDOC_TH_PROJECTIDS', 'Projects');
+	define('_AM_PHPDOC_TH_VERSIONIDS', 'Versions');
+	define('_AM_PHPDOC_TH_FILESIDS', 'Files');
+	define('_AM_PHPDOC_TH_NAME', 'Name');
+	define('_AM_PHPDOC_TH_EXTENDS', 'Extends');
+	define('_AM_PHPDOC_TH_EXTENDSCLASSID', 'Extended Class ID');
+	define('_AM_PHPDOC_TH_VARIABLES', 'Variables');
+	define('_AM_PHPDOC_TH_FUNCTIONID', 'Function ID');
+	define('_AM_PHPDOC_TH_CLASSIDS', 'Classes');
+	define('_AM_PHPDOC_TH_MODE', 'Mode');
+	define('_AM_PHPDOC_TH_RETURN', 'Return variable');
+	define('_AM_PHPDOC_TH_CALL', 'Function call');
+	define('_AM_PHPDOC_TH_VARIABLEID', 'Variable ID');
+	define('_AM_PHPDOC_TH_FUNCTIONIDS', 'Functions');
+	define('_AM_PHPDOC_TH_DEFAULT', 'Default value');
+	define('_AM_PHPDOC_TH_TYPE', 'Type');
+	define('_AM_PHPDOC_TH_PATHID', 'Path ID');
+	define('_AM_PHPDOC_TH_FOLDER', 'Folder');
+	define('_AM_PHPDOC_TH_RELATIVE', 'Relative To');
+	define('_AM_PHPDOC_TH_ITEMDIGESTID', 'Digest ID');
+	define('_AM_PHPDOC_TH_MENU', 'Menu Title');
+	define('_AM_PHPDOC_TH_TITLE', 'Title');
+	define('_AM_PHPDOC_TH_DESCRIPTION', 'Description');
+	define('_AM_PHPDOC_TH_LANGUAGE', 'Language');
+	
+	?>
