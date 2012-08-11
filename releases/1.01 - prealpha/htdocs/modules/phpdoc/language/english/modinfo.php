59bead03bc05d5a06911dd49c177cefa86953d4b
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-08-11 12:46:10 +0000

    Removing spaces from "define", which is required for Transifex per DCrusader

--- releases/1.01 - prealpha/htdocs/modules/phpdoc/language/english/modinfo.php
+++ releases/1.01 - prealpha/htdocs/modules/phpdoc/language/english/modinfo.php
@@ -1,137 +1,137 @@
 <?php
 
 	// Admin Menus
-	define('_MI_PHPDOC_ADMIN_MENU1', 'New Project Wizard');
-	define('_MI_PHPDOC_ADMIN_MENU2', 'Categories');
-	define('_MI_PHPDOC_ADMIN_MENU3', 'Projects');
-	define('_MI_PHPDOC_ADMIN_MENU4', 'Files');
-	define('_MI_PHPDOC_ADMIN_MENU5', 'Classes');
-	define('_MI_PHPDOC_ADMIN_MENU6', 'Functions');
-	define('_MI_PHPDOC_ADMIN_MENU7', 'Variables');
-	define('_MI_PHPDOC_ADMIN_MENU8', 'Paths');
-	define('_MI_PHPDOC_ADMIN_MENU9', 'Language Digest');
-	define('_MI_PHPDOC_ADMIN_MENU10', 'File Types');
-	define('_MI_PHPDOC_ADMIN_MENU11', 'Permissions');
+	define('_MI_PHPDOC_ADMIN_MENU1','New Project Wizard');
+	define('_MI_PHPDOC_ADMIN_MENU2','Categories');
+	define('_MI_PHPDOC_ADMIN_MENU3','Projects');
+	define('_MI_PHPDOC_ADMIN_MENU4','Files');
+	define('_MI_PHPDOC_ADMIN_MENU5','Classes');
+	define('_MI_PHPDOC_ADMIN_MENU6','Functions');
+	define('_MI_PHPDOC_ADMIN_MENU7','Variables');
+	define('_MI_PHPDOC_ADMIN_MENU8','Paths');
+	define('_MI_PHPDOC_ADMIN_MENU9','Language Digest');
+	define('_MI_PHPDOC_ADMIN_MENU10','File Types');
+	define('_MI_PHPDOC_ADMIN_MENU11','Permissions');
 
 	// Enumerators
-	define('_MI_PHPDOC_MODE_PUBLIC', 'Public');
-	define('_MI_PHPDOC_MODE_PRIVATE', 'Private');
-	define('_MI_PHPDOC_MODE_PROTECTED', 'Protected');
+	define('_MI_PHPDOC_MODE_PUBLIC','Public');
+	define('_MI_PHPDOC_MODE_PRIVATE','Private');
+	define('_MI_PHPDOC_MODE_PROTECTED','Protected');
 	
-	define('_MI_PHPDOC_TYPE_MIXED', 'Mixed Value');
-	define('_MI_PHPDOC_TYPE_INTEGER', 'Integer');
-	define('_MI_PHPDOC_TYPE_LONG', 'Long');
-	define('_MI_PHPDOC_TYPE_DOUBLE', 'Double');
-	define('_MI_PHPDOC_TYPE_FLOAT', 'Float');
-	define('_MI_PHPDOC_TYPE_STRING', 'String');
-	define('_MI_PHPDOC_TYPE_ARRAY', 'Array');
-	define('_MI_PHPDOC_TYPE_OBJECT', 'Object');
-	define('_MI_PHPDOC_TYPE_BOOLEAN', 'Boolean');
+	define('_MI_PHPDOC_TYPE_MIXED','Mixed Value');
+	define('_MI_PHPDOC_TYPE_INTEGER','Integer');
+	define('_MI_PHPDOC_TYPE_LONG','Long');
+	define('_MI_PHPDOC_TYPE_DOUBLE','Double');
+	define('_MI_PHPDOC_TYPE_FLOAT','Float');
+	define('_MI_PHPDOC_TYPE_STRING','String');
+	define('_MI_PHPDOC_TYPE_ARRAY','Array');
+	define('_MI_PHPDOC_TYPE_OBJECT','Object');
+	define('_MI_PHPDOC_TYPE_BOOLEAN','Boolean');
 	
-	define('_MI_PHPDOC_STATUS_ALPHA', 'Alpha');
-	define('_MI_PHPDOC_STATUS_BETA', 'Beta');
-	define('_MI_PHPDOC_STATUS_RC', 'Release Candidate');
-	define('_MI_PHPDOC_STATUS_STABLE', 'Stable');
-	define('_MI_PHPDOC_STATUS_MATURE', 'Mature');
-	define('_MI_PHPDOC_STATUS_EXPERIMENTAL', 'Experimental');
+	define('_MI_PHPDOC_STATUS_ALPHA','Alpha');
+	define('_MI_PHPDOC_STATUS_BETA','Beta');
+	define('_MI_PHPDOC_STATUS_RC','Release Candidate');
+	define('_MI_PHPDOC_STATUS_STABLE','Stable');
+	define('_MI_PHPDOC_STATUS_MATURE','Mature');
+	define('_MI_PHPDOC_STATUS_EXPERIMENTAL','Experimental');
 	
-	define('_MI_PHPDOC_TYPE_CATEGORY', 'Category Type');
-	define('_MI_PHPDOC_TYPE_VERSION', 'Version Type');
-	define('_MI_PHPDOC_TYPE_PROJECT', 'Project Type');
-	define('_MI_PHPDOC_TYPE_PATH', 'Path Type');
-	define('_MI_PHPDOC_TYPE_FILE', 'File Type');
-	define('_MI_PHPDOC_TYPE_FUNCTION', 'Function Type');
-	define('_MI_PHPDOC_TYPE_CLASS', 'Class Type');
-	define('_MI_PHPDOC_TYPE_VARIABLE', 'Variable Type');
-	define('_MI_PHPDOC_TYPE_OTHER', 'Other Type');
+	define('_MI_PHPDOC_TYPE_CATEGORY','Category Type');
+	define('_MI_PHPDOC_TYPE_VERSION','Version Type');
+	define('_MI_PHPDOC_TYPE_PROJECT','Project Type');
+	define('_MI_PHPDOC_TYPE_PATH','Path Type');
+	define('_MI_PHPDOC_TYPE_FILE','File Type');
+	define('_MI_PHPDOC_TYPE_FUNCTION','Function Type');
+	define('_MI_PHPDOC_TYPE_CLASS','Class Type');
+	define('_MI_PHPDOC_TYPE_VARIABLE','Variable Type');
+	define('_MI_PHPDOC_TYPE_OTHER','Other Type');
 	
-	define('_MI_PHPDOC_XOOPS_ROOT_PATH', 'Site root path');
-	define('_MI_PHPDOC_XOOPS_UPLOAD_PATH', 'Site upload path');
-	define('_MI_PHPDOC_XOOPS_VAR_PATH', 'Site data path');
-	define('_MI_PHPDOC_XOOPS_TRUST_PATH', 'Site library path');
-	define('_MI_PHPDOC_XOOPS_MODULE_PATH', 'Module Path');
-	define('_MI_PHPDOC_PROJECTS_PATH', 'Projects Folder');
-	define('_MI_PHPDOC_OPEN_PATH', 'Open Path');
+	define('_MI_PHPDOC_XOOPS_ROOT_PATH','Site root path');
+	define('_MI_PHPDOC_XOOPS_UPLOAD_PATH','Site upload path');
+	define('_MI_PHPDOC_XOOPS_VAR_PATH','Site data path');
+	define('_MI_PHPDOC_XOOPS_TRUST_PATH','Site library path');
+	define('_MI_PHPDOC_XOOPS_MODULE_PATH','Module Path');
+	define('_MI_PHPDOC_PROJECTS_PATH','Projects Folder');
+	define('_MI_PHPDOC_OPEN_PATH','Open Path');
 	
-	define('_MI_PHPDOC_FILETYPE_PHP', 'PHP File');
-	define('_MI_PHPDOC_FILETYPE_CSS', 'Stylesheet');
-	define('_MI_PHPDOC_FILETYPE_HTML', 'HTML File');
-	define('_MI_PHPDOC_FILETYPE_JAVA', 'Javascript File');
-	define('_MI_PHPDOC_FILETYPE_TXT', 'Text File');
-	define('_MI_PHPDOC_FILETYPE_IMAGE', 'Image');
-	define('_MI_PHPDOC_FILETYPE_ASSET', 'Asset File');
-	define('_MI_PHPDOC_FILETYPE_OTHER', 'Other File');
+	define('_MI_PHPDOC_FILETYPE_PHP','PHP File');
+	define('_MI_PHPDOC_FILETYPE_CSS','Stylesheet');
+	define('_MI_PHPDOC_FILETYPE_HTML','HTML File');
+	define('_MI_PHPDOC_FILETYPE_JAVA','Javascript File');
+	define('_MI_PHPDOC_FILETYPE_TXT','Text File');
+	define('_MI_PHPDOC_FILETYPE_IMAGE','Image');
+	define('_MI_PHPDOC_FILETYPE_ASSET','Asset File');
+	define('_MI_PHPDOC_FILETYPE_OTHER','Other File');
 	
 	// Plug-in ACL
-	define('_MI_PHPDOC_MODE_PUBLIC_PLUGIN', 'Public');
-	define('_MI_PHPDOC_MODE_PRIVATE_PLUGIN', 'Private');
-	define('_MI_PHPDOC_MODE_PROTECTED_PLUGIN', 'Protected');
+	define('_MI_PHPDOC_MODE_PUBLIC_PLUGIN','Public');
+	define('_MI_PHPDOC_MODE_PRIVATE_PLUGIN','Private');
+	define('_MI_PHPDOC_MODE_PROTECTED_PLUGIN','Protected');
 	
-	define('_MI_PHPDOC_TYPE_MIXED_PLUGIN', 'Mixed');
-	define('_MI_PHPDOC_TYPE_INTEGER_PLUGIN', 'Integer');
-	define('_MI_PHPDOC_TYPE_LONG_PLUGIN', 'Long');
-	define('_MI_PHPDOC_TYPE_DOUBLE_PLUGIN', 'Double');
-	define('_MI_PHPDOC_TYPE_FLOAT_PLUGIN', 'Float');
-	define('_MI_PHPDOC_TYPE_STRING_PLUGIN', 'String');
-	define('_MI_PHPDOC_TYPE_ARRAY_PLUGIN', 'Array');
-	define('_MI_PHPDOC_TYPE_OBJECT_PLUGIN', 'Object');
-	define('_MI_PHPDOC_TYPE_BOOLEAN_PLUGIN', 'Boolean');
+	define('_MI_PHPDOC_TYPE_MIXED_PLUGIN','Mixed');
+	define('_MI_PHPDOC_TYPE_INTEGER_PLUGIN','Integer');
+	define('_MI_PHPDOC_TYPE_LONG_PLUGIN','Long');
+	define('_MI_PHPDOC_TYPE_DOUBLE_PLUGIN','Double');
+	define('_MI_PHPDOC_TYPE_FLOAT_PLUGIN','Float');
+	define('_MI_PHPDOC_TYPE_STRING_PLUGIN','String');
+	define('_MI_PHPDOC_TYPE_ARRAY_PLUGIN','Array');
+	define('_MI_PHPDOC_TYPE_OBJECT_PLUGIN','Object');
+	define('_MI_PHPDOC_TYPE_BOOLEAN_PLUGIN','Boolean');
 
-	define('_MI_PHPDOC_STATUS_ALPHA_PLUGIN', 'Alpha');
-	define('_MI_PHPDOC_STATUS_BETA_PLUGIN', 'Beta');
-	define('_MI_PHPDOC_STATUS_RC_PLUGIN', 'RC');
-	define('_MI_PHPDOC_STATUS_STABLE_PLUGIN', 'Stable');
-	define('_MI_PHPDOC_STATUS_MATURE_PLUGIN', 'Mature');
-	define('_MI_PHPDOC_STATUS_EXPERIMENTAL_PLUGIN', 'Experimental');
+	define('_MI_PHPDOC_STATUS_ALPHA_PLUGIN','Alpha');
+	define('_MI_PHPDOC_STATUS_BETA_PLUGIN','Beta');
+	define('_MI_PHPDOC_STATUS_RC_PLUGIN','RC');
+	define('_MI_PHPDOC_STATUS_STABLE_PLUGIN','Stable');
+	define('_MI_PHPDOC_STATUS_MATURE_PLUGIN','Mature');
+	define('_MI_PHPDOC_STATUS_EXPERIMENTAL_PLUGIN','Experimental');
 	
-	define('_MI_PHPDOC_TYPE_CATEGORY_PLUGIN', 'Category');
-	define('_MI_PHPDOC_TYPE_VERSION_PLUGIN', 'Version');
-	define('_MI_PHPDOC_TYPE_PROJECT_PLUGIN', 'Project');
-	define('_MI_PHPDOC_TYPE_PATH_PLUGIN', 'Path');
-	define('_MI_PHPDOC_TYPE_FILE_PLUGIN', 'File');
-	define('_MI_PHPDOC_TYPE_FUNCTION_PLUGIN', 'Function');
-	define('_MI_PHPDOC_TYPE_CLASS_PLUGIN', 'Class');
-	define('_MI_PHPDOC_TYPE_VARIABLE_PLUGIN', 'Variable');
-	define('_MI_PHPDOC_TYPE_OTHER_PLUGIN', 'Other');
+	define('_MI_PHPDOC_TYPE_CATEGORY_PLUGIN','Category');
+	define('_MI_PHPDOC_TYPE_VERSION_PLUGIN','Version');
+	define('_MI_PHPDOC_TYPE_PROJECT_PLUGIN','Project');
+	define('_MI_PHPDOC_TYPE_PATH_PLUGIN','Path');
+	define('_MI_PHPDOC_TYPE_FILE_PLUGIN','File');
+	define('_MI_PHPDOC_TYPE_FUNCTION_PLUGIN','Function');
+	define('_MI_PHPDOC_TYPE_CLASS_PLUGIN','Class');
+	define('_MI_PHPDOC_TYPE_VARIABLE_PLUGIN','Variable');
+	define('_MI_PHPDOC_TYPE_OTHER_PLUGIN','Other');
 
-	define('_MI_PHPDOC_XOOPS_ROOT_PATH_PLUGIN', 'RootPath');
-	define('_MI_PHPDOC_XOOPS_UPLOAD_PATH_PLUGIN', 'UploadPath');
-	define('_MI_PHPDOC_XOOPS_VAR_PATH_PLUGIN', 'DataPath');
-	define('_MI_PHPDOC_XOOPS_TRUST_PATH_PLUGIN', 'LibraryPath');
-	define('_MI_PHPDOC_XOOPS_MODULE_PATH_PLUGIN', 'ModulePath');
-	define('_MI_PHPDOC_PROJECTS_PATH_PLUGIN', 'ProjectsPath');
-	define('_MI_PHPDOC_OPEN_PATH_PLUGIN', 'OpenPath');
+	define('_MI_PHPDOC_XOOPS_ROOT_PATH_PLUGIN','RootPath');
+	define('_MI_PHPDOC_XOOPS_UPLOAD_PATH_PLUGIN','UploadPath');
+	define('_MI_PHPDOC_XOOPS_VAR_PATH_PLUGIN','DataPath');
+	define('_MI_PHPDOC_XOOPS_TRUST_PATH_PLUGIN','LibraryPath');
+	define('_MI_PHPDOC_XOOPS_MODULE_PATH_PLUGIN','ModulePath');
+	define('_MI_PHPDOC_PROJECTS_PATH_PLUGIN','ProjectsPath');
+	define('_MI_PHPDOC_OPEN_PATH_PLUGIN','OpenPath');
 	
-	define('_MI_PHPDOC_FILETYPE_PHP_PLUGIN', 'PHP');
-	define('_MI_PHPDOC_FILETYPE_CSS_PLUGIN', 'Stylesheet');
-	define('_MI_PHPDOC_FILETYPE_HTML_PLUGIN', 'HTML');
-	define('_MI_PHPDOC_FILETYPE_JAVA_PLUGIN', 'Javascript');
-	define('_MI_PHPDOC_FILETYPE_TXT_PLUGIN', 'Text');
-	define('_MI_PHPDOC_FILETYPE_IMAGE_PLUGIN', 'Image');
-	define('_MI_PHPDOC_FILETYPE_ASSET_PLUGIN', 'Asset');
-	define('_MI_PHPDOC_FILETYPE_OTHER_PLUGIN', 'Other');
+	define('_MI_PHPDOC_FILETYPE_PHP_PLUGIN','PHP');
+	define('_MI_PHPDOC_FILETYPE_CSS_PLUGIN','Stylesheet');
+	define('_MI_PHPDOC_FILETYPE_HTML_PLUGIN','HTML');
+	define('_MI_PHPDOC_FILETYPE_JAVA_PLUGIN','Javascript');
+	define('_MI_PHPDOC_FILETYPE_TXT_PLUGIN','Text');
+	define('_MI_PHPDOC_FILETYPE_IMAGE_PLUGIN','Image');
+	define('_MI_PHPDOC_FILETYPE_ASSET_PLUGIN','Asset');
+	define('_MI_PHPDOC_FILETYPE_OTHER_PLUGIN','Other');
 	
 	// XOOPS Version
-	define('_MI_PHPDOC_NAME', 'PHP Documentor');
-	define('_MI_PHPDOC_DESCRIPTION', 'PHP Documentor is a module for XOOPS which will import your PHP Code in projects and allow it to be documented!');
-	define('_MI_PHPDOC_DIRNAME', 'phpdoc');
+	define('_MI_PHPDOC_NAME','PHP Documentor');
+	define('_MI_PHPDOC_DESCRIPTION','PHP Documentor is a module for XOOPS which will import your PHP Code in projects and allow it to be documented!');
+	define('_MI_PHPDOC_DIRNAME','phpdoc');
 	
 	// Preferences
-	define('_MI_PHPDOC_PROJECTS_PATH', 'Projects Upload Path');
-	define('_MI_PHPDOC_PROJECTS_PATH_DESC', 'Folder which has subfolders containing projects for documenting (no trailing slash).');
-	define('_MI_PHPDOC_HTACCESS', '.htaccess SEO');
-	define('_MI_PHPDOC_HTACCESS_DESC', 'See docs/.htaccess for SEO Options');
-	define('_MI_PHPDOC_BASEURL', 'SEO Base URL');
-	define('_MI_PHPDOC_BASEURL_DESC', 'Base of URL for SEO with .htaccess');
-	define('_MI_PHPDOC_ENDOFURL', 'SEO End of URL');
-	define('_MI_PHPDOC_ENDOFURL_DESC', 'End of URL for SEO with .htaccess');
-	define('_MI_PHPDOC_ENDOFURLRSS', 'SEO End of RSS URL');
-	define('_MI_PHPDOC_ENDOFURLRSS_DESC', 'End of RSS Content URL for SEO with .htaccess');
-	define('_MI_PHPDOC_ENDOFURLPDF', 'SEO End of PDF URL');
-	define('_MI_PHPDOC_ENDOFURLPDF_DESC', 'End of PDF Content URL for SEO with .htaccess');
-	define('_MI_PHPDOC_EDITOR', 'Editor to use');
-	define('_MI_PHPDOC_EDITOR_DESC', 'This is the editor that will be use');
+	define('_MI_PHPDOC_PROJECTS_PATH','Projects Upload Path');
+	define('_MI_PHPDOC_PROJECTS_PATH_DESC','Folder which has subfolders containing projects for documenting (no trailing slash).');
+	define('_MI_PHPDOC_HTACCESS','.htaccess SEO');
+	define('_MI_PHPDOC_HTACCESS_DESC','See docs/.htaccess for SEO Options');
+	define('_MI_PHPDOC_BASEURL','SEO Base URL');
+	define('_MI_PHPDOC_BASEURL_DESC','Base of URL for SEO with .htaccess');
+	define('_MI_PHPDOC_ENDOFURL','SEO End of URL');
+	define('_MI_PHPDOC_ENDOFURL_DESC','End of URL for SEO with .htaccess');
+	define('_MI_PHPDOC_ENDOFURLRSS','SEO End of RSS URL');
+	define('_MI_PHPDOC_ENDOFURLRSS_DESC','End of RSS Content URL for SEO with .htaccess');
+	define('_MI_PHPDOC_ENDOFURLPDF','SEO End of PDF URL');
+	define('_MI_PHPDOC_ENDOFURLPDF_DESC','End of PDF Content URL for SEO with .htaccess');
+	define('_MI_PHPDOC_EDITOR','Editor to use');
+	define('_MI_PHPDOC_EDITOR_DESC','This is the editor that will be use');
 	
 	// Form Variables
-	define('_MI_PHPDOC_NONE', 'None');
+	define('_MI_PHPDOC_NONE','None');
 ?>
